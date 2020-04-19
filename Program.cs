d240c0a2a6e1f601d4906ac2a26acb52488720a3
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-19 15:01:01 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        //Funtion factorial
+        static BigInteger Factorial(uint num)
+        {
+            BigInteger f = 1;
+            for (uint i = num; i > 0; i--)
+            {
+                f *= i;
+            }
+
+            return f;
+        }
+        static void Main(string[] args)
+        {
+            //Declaring variables
+            BigInteger  Cn = 1;
+            uint n = 0, numerator = 0, denominatorSum = 0;
+
+
+            //Requesting numerator and denominator
+            do
+            {
+                Console.Write(" Enter the number: ");
+            n = Convert.ToUInt32 (Console.ReadLine());
+                           
+            }
+            while (n <= 0);
+
+            //calculating the Catalan number
+            numerator = 2 * n;
+            denominatorSum = n + 1;
+
+            Cn = Factorial(numerator) / (Factorial(denominatorSum) * Factorial(n));
+
+            Console.Write(" Cn = (2*{0})! / ({0} + 1)! * {0}! = {1}", n, Cn);
+
+            Console.ReadKey();
+        }
+    }
+}
